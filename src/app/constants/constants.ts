1f67937238a93abb20fa69ef385ac4e581c47330
Aditya Bhave <adi007me@gmail.com>
2020-04-14 13:34:15 +0530

    Updated Google ID

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -3,7 +3,7 @@ import { environment } from "../../environments/environment";
 export class Constants {
     public static get BASE_URL(): string {
         if (environment.production) {
-            return 'https://3000-f6a84fe5-7a94-45ef-a49e-c3c0939547c1.ws-ap01.gitpod.io';
+            return 'https://social-distancer-api.herokuapp.com';
         } else {
             return 'http://localhost:3000';
         }
